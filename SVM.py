bb80715f5fc43f8f00ad457d42f9461d547bccab
mikirui <545559910@qq.com>
2016-06-08 14:49:58 +0800

    adjust the parameter/RBF

--- SVM.py
+++ SVM.py
@@ -2,7 +2,7 @@ import cv2
 import random
 import numpy as np
 
-feature_path = "feature.txt"
+feature_path = "feature2.txt"
 
 #1.read data
 all_data = []
@@ -49,7 +49,7 @@ def Score(svm,paras):
 	train_size = size/8*7
 	val_size = size/8
 	score = 0.0
-	print("train_size is %d, val_size is %d" % (train_size,val_size))
+	#print("train_size is %d, val_size is %d" % (train_size,val_size))
 	for i in range(0,8):             #seperate data
 		train_data = []
 		train_label = []
@@ -65,21 +65,21 @@ def Score(svm,paras):
 		for j in range(st+val_size,size):
 			train_data.append(shuffle_data[j][:])
 			train_label.append(shuffle_label[j])
-		print("train_size is %d, val_size is %d" % (len(train_data),len(val_data)))
+		#print("train_size is %d, val_size is %d" % (len(train_data),len(val_data)))
 		train_x = np.array(train_data,dtype = np.float32)
 		train_y = np.array(train_label,dtype = np.float32)
 		val_x = np.array(val_data,dtype = np.float32)
 		val_label = np.array(val_label,dtype = np.float32)
-		print("svm training")
+		#print("svm training")
 		svm.train(train_x,train_y,params = paras)
-		print("svm train finish")
-		print(val_x.shape)
+		#print("svm train finish")
+		#print(val_x.shape)
 		cnt = 0
 		for j in range(0,val_x.shape[0]):
 			pred = svm.predict(val_x[j]) 
 			if(pred == val_label[j]):
 				cnt += 1
-		print("predict correct %d times" % (cnt))
+		print("%dth batch: predict correct %d times" % (i,cnt))
 		score += float(cnt)/val_size
 	score = score/K
 	return score
@@ -93,24 +93,36 @@ svm = cv2.SVM()
 s = Score(svm,svm_params)
 print("C = %d ,gamma = %d, score = %f" % (C,gamma,s))
 '''
-C_grid = [i for i in range(1,10)]
-gamma_grid = [i for i in range(1,10)]
+C_grid = [1.0,2.0]
+gamma_grid = [2.0**(-4)]
 max_score = 0
 best_C, best_g = 0, 0
 
+print(C_grid)
+
 for C in C_grid:
 	for gamma in gamma_grid:
-		svm_params = dict(kernel_type = cv2.SVM_LINEAR,svm_type = cv2.SVM_C_SVC,
+		svm_params = dict(kernel_type = cv2.SVM_RBF,svm_type = cv2.SVM_C_SVC,
 			C=C, gamma = gamma)
 		svm = cv2.SVM()
 		s = Score(svm,svm_params)
-		print("C = %d ,gamma = %d, score = %f" % (C,gamma,s))
+		print("C = %f ,gamma = %f, score = %f" % (C,gamma,s))
 		if(s>max_score):
 			max_score = s
 			best_C = C
 			best_g = gamma
 
-print("Best perfomance:\nC = %d ,gamma = %d, score = %f" % (C,gamma,s))
+print("Best perfomance:\nC = %f ,gamma = %f, score = %f" % (best_C,best_g,max_score))
+'''
+for C in C_grid:
+	svm_params = dict(kernel_type = cv2.SVM_LINEAR,svm_type = cv2.SVM_C_SVC,
+		C=C)
+	svm = cv2.SVM()
+	s = Score(svm,svm_params)
+	print("C = %f , score = %f" % (C,s))
+	if(s>max_score):
+		max_score = s
+		best_C = C
 
-
-
+print("Best perfomance:\nC = %f, score = %f" % (best_C,max_score))
+'''
